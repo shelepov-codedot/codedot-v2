68025e9a43475cd6f72f4ddd544e5de212f4ef13
Vadim <vadimm.crocode@gmail.com>
2023-08-07 16:33:47 +0300

    init project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 </header>
 <main>
   <h2>Pets</h2>
-  {#if pets && pets.length}
+  {#if data.pets && data.pets.length}
   <ul>
-    {#each pets as animal}
+    {#each data.pets as animal}
     <li>{animal.name}</li>
     {/each}
   </ul>
@@ -14,9 +14,9 @@
   {/if}
   <h2>Data</h2>
   <div>
-    {#if pets && pets.length}
+    {#if data.pets && data.pets.length}
     <pre>
-    {JSON.stringify(pets, null, 2)}
+    {JSON.stringify(data.pets, null, 2)}
     </pre>
     {:else}
     <p>Your data will show up here when you've configured everything correctly</p>
@@ -25,14 +25,5 @@
 </main>
 
 <script>
-  let pets = [
-    /* {
-    _createdAt: "2022-03-08T09:28:00Z",
-    _id: "1f69c53d-418a-452f-849a-e92466bb9c75",
-    _rev: "xnBg0xhUDzo561jnWODd5e",
-    _type: "pet",
-    _updatedAt: "2022-03-08T09:28:00Z",
-    name: "Capybara"
-  } */
-  ];
+  export let data;
 </script>
---
+++ studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'c6ki8epl',
+    dataset: 'production'
+  }
+})
