4b00aa9627cceadac64a57a8f8bddc014bc314a6
Daniil <yurkevich.crocode@gmail.com>
2023-10-13 12:37:34 +0300

    try added second form

--- src/routes/netlifyform/+page.svelte
+++ src/routes/netlifyform/+page.svelte
@@ -10,7 +10,50 @@
   netlify-honeypot="infoo"
   style="opacity:0; height:0; overflow: hidden; width:0;"
 >
-  <!-- <input type="hidden" name="form-name" value="contact-form-form-netlify" /> -->
+  <div class="contact-form__form-wrapper">
+    <div class="contact-form__input-wrapper">
+      <label for="name" class="contact-form__label">Your name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="name*"
+        class="contact-form__input"
+        required
+      />
+    </div>
+    <div class="contact-form__input-wrapper">
+      <label for="email" class="contact-form__label">Your email</label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        placeholder="email*"
+        class="contact-form__input"
+        required
+      />
+    </div>
+  </div>
+  <div class="contact-form__form-wrapper contact-form__form-wrapper--column">
+    <div class="contact-form__input-wrapper">
+      <label for="message" class="contact-form__label">Tell us about your project</label>
+      <textarea name="message" id="message" rows="5" placeholder="ss" class="contact-form__input" />
+    </div>
+    <div class="contact-form__button-wrapper">
+      <input type="submit" value="ss" class="btn" />
+      <p class="contact-form__form-policy">ss</p>
+    </div>
+  </div>
+</form>
+
+<form
+  method="POST"
+  class="contact-form__form"
+  name="cv-form"
+  data-netlify="true"
+  netlify-honeypot="cv-form"
+  style="opacity:0; height:0; overflow: hidden; width:0;"
+>
   <div class="contact-form__form-wrapper">
     <div class="contact-form__input-wrapper">
       <label for="name" class="contact-form__label">Your name</label>
